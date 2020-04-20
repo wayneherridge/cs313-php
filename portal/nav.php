c1a04aa629b2870683b70bfbb6ac2686fa690dd1
wayneherridge <wsherridge@icloud.com>
2020-04-20 17:52:11 +0100

    changed procfile back for marking

--- portal/nav.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<a href="index.php">About Me</a>'
-'<a href="assignments.php">Assignments</a>';
-?>
---
+++ portal/shared/nav.php
@@ -0,0 +1,6 @@
+<nav>
+  <ul>
+    <li><a href="index.php">About Me</a></li>
+    <li><a href="assignments.php">My Assignments</a></li>
+  </ul>
+</nav>
