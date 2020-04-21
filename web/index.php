3f739b9e2a2d355b6802c965535ee77dbf7322e0
wayneherridge <wsherridge@icloud.com>
2020-04-21 20:15:45 +0100

    moved my assignment links site into main folder

--- web/index.php
+++ web/index.php
@@ -1,5 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+  <body>
+    <!-- header.php -->
+    <?php include 'header.php' ?>
 
-  phpinfo();
+    <!-- nav.php -->
+    <?php include 'nav.php'; ?>
 
-?>
+    <main>
+      <section class="hero">
+        <figure>
+          <img src="" alt="hero">
+        </figure>
+      </section>
+
+      <section>
+          <h3>Welcome Section</h3>
+      </section>
+
+    </main>
+
+    <!-- footer.php -->
+    <?php include 'footer.php';?>
+
+    <!-- link to JS scripts -->
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
