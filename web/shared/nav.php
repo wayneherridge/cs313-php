900a67b76f5e55a20329a620a211d37b9636026e
wayneherridge <wsherridge@icloud.com>
2020-04-21 20:28:45 +0100

    fixed hamburger menu

--- web/shared/nav.php
+++ web/shared/nav.php
@@ -1,5 +1,5 @@
   <nav class="Clearfix">
-    <button onclick="toggleMenu()">&49776;</button>
+    <button onclick="toggleMenu()">&#9776;</button>
     <ul id="primaryNav" class="hide">
       <li class="active"><a href="index.php">About Me</a></li>
       <li><a href="assignments.php">My Assignments</a></li>
