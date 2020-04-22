1e44796e1c9835f0eeec787ed11ca0368449fcd8
wayneherridge <wsherridge@icloud.com>
2020-04-22 17:02:17 +0100

    fixed links for assignment page

--- web/assignments.php
+++ web/assignments.php
@@ -2,31 +2,28 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <title>All About Me | Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignments</title>
+
+    <!-- Stylesheet Links -->
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/normalize.css">
-
   </head>
   <body>
-    <div class="grid-container">
+
     <!-- header.php -->
     <?php include 'shared/header.php' ?>
 
-    <main class="main">
-      <figure>
-        <img src="" alt="Under Construction">
-      </figure>
-      <h1>Under Construction</h1>
-      <hr>
-      <h3>Coming Soon!</h3>
+
+    <main>
+      <h1>welcome</h1>
+      <p></p>
     </main>
-</div>
+
     <!-- footer.php -->
     <?php include 'shared/footer.php';?>
 
     <!-- link to JS scripts -->
     <script src="js/scripts.js"></script>
-    
   </body>
 </html>
