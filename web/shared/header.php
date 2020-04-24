d3e728b2c623c0108ce63a133df73cd2d2674c9b
wayneherridge <wsherridge@icloud.com>
2020-04-24 19:31:58 +0100

    working on layout

--- web/shared/header.php
+++ web/shared/header.php
@@ -1,7 +1,12 @@
-<header>
-  <figure>
-    <img src="" alt="logo">
-  </figure>
-  <h1>Wayne S. Herridge</h1>
-  <h2>Me, My Assignments, and I</h2>
+<header class="header-container">
+  <div class="logo">
+    <figure>
+      <img src="" alt="logo">
+    </figure>
+  </div>
+  <div class="site-title">
+    <h1>Wayne S. Herridge</h1>
+    <h2>Me, My Assignments, and I</h2>
+  </div>
+
 </header>
