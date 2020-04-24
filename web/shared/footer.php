0cb73375fcb0fa1f4ff507409936da6e7c4af291
wayneherridge <wsherridge@icloud.com>
2020-04-24 23:56:01 +0100

    working on layout

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,3 +1,9 @@
-<footer>
-    <?php echo "<p>&copy;" . date("Y") . " Wayne Herridge</p>";?>
+<!-- Footer -->
+<footer class="page-footer font-small blue fixed-bottom">
+
+  <!-- Copyright -->
+  <?php echo "<p>&copy;" . date("Y") . " Wayne Herridge</p>";?>
+  <!-- Copyright -->
+
 </footer>
+<!-- Footer -->
