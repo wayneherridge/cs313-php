dc209d27c49c215e2dbd287ed670afa17ebc1c14
wayneherridge <wsherridge@icloud.com>
2020-04-29 22:00:44 +0100

    updated footer

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -8,3 +8,9 @@
 
 </footer>
 <!-- Footer -->
+
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
