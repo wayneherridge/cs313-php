60af38892b07723b4bd448540de494b461e3e5f4
wayneherridge <wsherridge@icloud.com>
2020-04-29 22:16:02 +0100

    updated site

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,12 +1,6 @@
 
-<footer class="footer mt-auto py-3">
+<footer class="footer mt-auto py-3 fixed-bottom">
   <div class="container">
     <span class="text-muted">© 2020 Copyright: Wayne Herridge</span>
   </div>
 </footer>
-
-<footer class="footer">
-      <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
-      </div>
-    </footer>
