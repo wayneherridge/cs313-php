f9314c86c21a31c26e77bea70254c9ae8002da07
wayneherridge <wsherridge@icloud.com>
2020-04-30 08:51:21 +0100

    updated font sizes

--- web/shared/nav.php
+++ web/shared/nav.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-sm navbar-light bg-info">
-  <a class="navbar-brand" href="#">Wayne S. Herridge</a>
+  <a class="navbar-brand text-light" href="#">Wayne S. Herridge</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link text-light" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="assignments.php">Assignments</a>
+        <a class="nav-link text-light" href="assignments.php">Assignments</a>
       </li>
     </ul>
   </div>
