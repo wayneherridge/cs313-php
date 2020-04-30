0021da9280301c1f7becd39fb6074ff9cefd51c0
wayneherridge <wsherridge@icloud.com>
2020-04-30 10:13:45 +0100

    updating navbar

--- web/shared/nav.php
+++ web/shared/nav.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-info">
   <a class="navbar-brand text-light" href="#">Wayne S. Herridge</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon text-light"></span>
+    <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link text-light" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link text-light" href="assignments.php">Assignments</a>
