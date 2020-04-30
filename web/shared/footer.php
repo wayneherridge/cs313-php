ae6cfd44605fff54fb4329fc9e914ca5658fbe0e
wayneherridge <wsherridge@icloud.com>
2020-04-30 09:39:35 +0100

    updating navbar colors

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,3 +1,9 @@
-<footer>
+<footer class="">
   <small>© 2020 Copyright: Wayne Herridge</small>
 </footer>
+
+<footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
