b7f9c3cbd9160e40178489a0bc68ee3efaa90307
wayneherridge <wsherridge@icloud.com>
2020-04-30 11:07:08 +0100

    updating footer

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
       <div class="container">
-        <span class="text-info">&copy; <?php echo date("Y"); ?></span>
+        <span class="text-info">&copy; <?php echo date("Y"); ?> | Wayne Herridge</span>
       </div>
     </footer>
