69195637612b39e9468a9b675c454b235bb062c6
wayneherridge <wsherridge@icloud.com>
2020-04-30 09:48:02 +0100

    updating colors

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer text-info">
       <div class="container">
         <span class="text-muted">&copy; 2020 Copyright: Wayne Herridge</span>
       </div>
