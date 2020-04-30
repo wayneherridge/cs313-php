7612d4620bd033c452a39dd0993c5ddb3c9e881e
wayneherridge <wsherridge@icloud.com>
2020-04-30 09:31:08 +0100

    updating navbar colors

--- web/shared/nav.php
+++ web/shared/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
   <a class="navbar-brand text-light" href="#">Wayne S. Herridge</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
