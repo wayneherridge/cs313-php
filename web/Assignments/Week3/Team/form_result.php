10901386dba930f35a935b812aa82de970c889cc
wayneherridge <wsherridge@icloud.com>
2020-05-04 21:45:16 +0100

    uploaded week2 team files

--- web/Assignments/Week3/Team/form_result.php
+++ web/Assignments/Week3/Team/form_result.php
@@ -1,15 +1,33 @@
 <?php 
-	
+	$name = htmlspecialchars($_POST["name"]);
+	$email = htmlspecialchars($_POST["email"]);
+	$major = htmlspecialchars($_POST["major"]);
+	$places = $_POST["places"];
+	$comments = htmlspecialchars($_POST["comments"]);
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>Form Results</title>
 	</head>
 
 <body>
+	<h3>Form Results</h3>
 
+	<p>Name: <?=$name ?></p>
+	<p>Email <a href="mailto:<?=$email ?>"><?=$email ?></a></p>
+	<p>major: <?=$major ?></p>
+	<p>destinations:</p>
+
+	<ul>
+		<?php
+		foreach ($places as $place) {
+		$place_clean = htmlspecialchars($place);
+		echo "<li><p>$place_clean</p></li>";
+		}
+		?>
+	</ul>
 </body>
 
 </html>
