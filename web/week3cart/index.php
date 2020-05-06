5bb39f40df1c9921d3850e79805c7953269a06a8
wayneherridge <wsherridge@icloud.com>
2020-05-06 13:49:36 +0100

    added cart files

---
+++ web/week3cart/index.php
@@ -0,0 +1,20 @@
+<?Php
+echo "<br><br><br><a href=cart.php>Cart with Products after adding</a> . <a href=cart-display.php>Display Items</a> . <a href=cart-remove.php>Remove Item by Selection</a> . <a href=cart-remove-all.php>Remove all Items</a> . <a href=cart-add.php>Add Product by User</a>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Store</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Store</a></li>
+            <li><a href="cart.php">View Cart</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
