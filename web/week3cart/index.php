8f17768649e2244949f4a5c4ef5bda3c7b164e36
wayneherridge <wsherridge@icloud.com>
2020-05-06 23:52:29 +0100

    added cart files

--- web/week3cart/index.php
+++ web/week3cart/index.php
@@ -142,13 +142,9 @@ $v = "1.0";
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">PHP-</a>
+          <a class="navbar-brand" href="index.php">PHP Shopping Cart</a>
         </div>
         <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-			<li class="active"><a href="/" target="blank">Who Am I</a></li>
-			<li class="active"><a href="https://github.com/ganbarli/PHP-" target="blank">GitHub Project Page</a></li>
-          </ul>
         </div><!-- /.nav-collapse -->
       </div><!-- /.container -->
     </div><!-- /.navbar -->
