3ab281795d47e1d04b045104ac5c5a23cbccb0d7
wayneherridge <wsherridge@icloud.com>
2020-05-06 10:45:14 +0100

    added cart files

---
+++ web/week3cart/menu.php
@@ -0,0 +1,7 @@
+<?Php
+echo "<br><br><br><a href=cart.php>Cart with Products after adding</a> . <a href=cart-display.php>Display Items</a> . <a href=cart-remove.php>Remove Item by Selection</a> . <a href=cart-remove-all.php>Remove all Items</a> . <a href=cart-add.php>Add Product by User</a>
+<br><br><br><br>
+Read more on <a href=http://www.plus2net.com/php_tutorial/array-session.php rel='nofollow'>Php Session Cart & Arrays</a> 
+
+";
+?>
