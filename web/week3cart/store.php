796cd72657b0d54756bfe381164bb9378d14272e
wayneherridge <wsherridge@icloud.com>
2020-05-08 16:30:37 +0100

    added cart files

--- web/week3cart/store.php
+++ web/week3cart/store.php
@@ -36,15 +36,13 @@
       <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Free</h4>
+            <h4 class="my-0 font-weight-normal">Red Shoes</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">$20</h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
+              <li>Size : UK 6</li>
+              <li>Color: Red</li>
             </ul>
             <button type="button" class="btn btn-lg btn-block btn-outline-primary">Add to Cart</button>
           </div>
@@ -69,9 +67,6 @@
             <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
             <ul class="list-unstyled mt-3 mb-4">
               <li>30 users included</li>
-              <li>15 GB of storage</li>
-              <li>Phone and email support</li>
-              <li>Help center access</li>
             </ul>
             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
           </div>
