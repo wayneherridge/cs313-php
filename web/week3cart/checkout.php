95e6da6054761532a85cb25f5ea01b6903e67b3d
wayneherridge <wsherridge@icloud.com>
2020-05-08 16:46:36 +0100

    added cart files

--- web/week3cart/checkout.php
+++ web/week3cart/checkout.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="css/form-validation.css" rel="stylesheet">
+    <link href="css/pricing.css" rel="stylesheet">
+
   </head>
 
   <body>
