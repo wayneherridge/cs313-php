29689ba95bcdea19683328a4ad487db0af8a518c
wayneherridge <wsherridge@icloud.com>
2020-05-08 16:36:20 +0100

    added cart files

--- web/week3cart/store.php
+++ web/week3cart/store.php
@@ -62,12 +62,13 @@
         </div>
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Enterprise</h4>
+            <h4 class="my-0 font-weight-normal">VE Day Coin</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+            <h1 class="card-title pricing-card-title">$100</h1>
             <ul class="list-unstyled mt-3 mb-4">
-              <li>30 users included</li>
+              <li>Size  : British 50 pence</li>
+              <li>Value : $100</li>
             </ul>
             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
           </div>
