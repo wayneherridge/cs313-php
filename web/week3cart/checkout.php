651c68a1e4e8d105e156ff0f3947a465fca00ff8
wayneherridge <wsherridge@icloud.com>
2020-05-08 17:26:44 +0100

    added cart files

--- web/week3cart/checkout.php
+++ web/week3cart/checkout.php
@@ -89,14 +89,6 @@
             </div>
 
             <div class="mb-3">
-              <label for="email">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" id="email" placeholder="you@example.com">
-              <div class="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
-
-            <div class="mb-3">
               <label for="address">Address</label>
               <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
               <div class="invalid-feedback">
