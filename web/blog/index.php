a3aa7fa49d4c7da70e3ff26064a7db703aa099aa
wayneherridge <wsherridge@icloud.com>
2020-05-21 21:04:42 +0100

    week5 files

--- web/blog/index.php
+++ web/blog/index.php
@@ -3,7 +3,6 @@
 include("./includes/config.php");
 
 $searchTerm = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_STRING);
-echo $searchTerm;
 
 require "./includes/dbconnect.php";
 $db = get_db();
@@ -11,7 +10,6 @@ $db = get_db();
 if (!empty($searchTerm)) {
     $statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%:searchTerm%'");
     $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
-    //$statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%te%'");
 } else {
     $statement = $db->prepare("SELECT title FROM posts");
 }
