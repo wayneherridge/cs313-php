61607db514bfa0b3aa792352496ea9083697be42
wayneherridge <wsherridge@icloud.com>
2020-05-21 21:27:56 +0100

    week5 files

--- web/blog/includes/config.php
+++ web/blog/includes/config.php
@@ -1,5 +1,9 @@
 <?php
 	switch ($_SERVER["SCRIPT_NAME"]) {
+		case "/details.php":
+			$CURRENT_PAGE = "Details"; 
+			$PAGE_TITLE = "Blog Details";
+			break;
 		case "/about.php":
 			$CURRENT_PAGE = "About"; 
 			$PAGE_TITLE = "About Us";
