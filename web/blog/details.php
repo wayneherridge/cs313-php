ed86719d4aefba155ced6be1e5c4cb5c856a1a5b
wayneherridge <wsherridge@icloud.com>
2020-05-21 21:37:07 +0100

    week5 files

--- web/blog/details.php
+++ web/blog/details.php
@@ -7,17 +7,17 @@ $searchTerm = filter_input(INPUT_GET, 'post', FILTER_SANITIZE_STRING);
 require "./includes/dbconnect.php";
 $db = get_db();
 
-// if (isset($searchTerm)) {
-//     $statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%:searchTerm%'");
-//     $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
-// } else {
-//     $statement = $db->prepare("SELECT title FROM posts");
-// }
+if (!empty($searchTerm)) {
+    $statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%:searchTerm%'");
+    $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
+    //$statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%te%'");
+} else {
+    $statement = $db->prepare("SELECT title FROM posts");
+}
     $statement->execute();
 	$blogposts = $statement->fetchAll(PDO::FETCH_ASSOC);
     
-    $statement->closeCursor();
-    
+	$statement->closeCursor();
 
 ?>
 
@@ -35,15 +35,7 @@ $db = get_db();
 <div class="container" id="main-content">
 	<h2>Blog Details</h2>
 
-<?php
-    foreach ($blogposts as $row)
-    {
-		$body = $row['body'];
-		
-        echo "<p><a href='index.php'><strong>$body</strong></a><p>";
-    }
 
-?>
 
 </div>
 
