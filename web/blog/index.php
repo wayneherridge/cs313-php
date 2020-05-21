5078c74d6f6f475e600fb6c97c3a2ac3e67b006f
wayneherridge <wsherridge@icloud.com>
2020-05-21 20:27:40 +0100

    week5 files

--- web/blog/index.php
+++ web/blog/index.php
@@ -3,18 +3,21 @@
 include("./includes/config.php");
 
 $searchTerm = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_STRING);
+echo $searchTerm;
 
 require "./includes/dbconnect.php";
 $db = get_db();
 
 if (!empty($searchTerm)) {
-    $statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%:searchTerm%'");
-    $statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
+    //$statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%:searchTerm%'");
+    //$statement->bindValue(':searchTerm', $searchTerm, PDO::PARAM_STR);
+    $statement = $db->prepare("SELECT * FROM posts WHERE title LIKE '%te%'");
 } else {
     $statement = $db->prepare("SELECT title FROM posts");
 }
 	$blogposts = $statement->fetchAll(PDO::FETCH_ASSOC);
-	$statement->execute();
+    
+    $statement->execute();
 	$statement->closeCursor();
 
 ?>
