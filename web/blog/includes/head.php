e8e50644b40415a3821e81ea0589d87c32b54d6b
wayneherridge <wsherridge@icloud.com>
2020-05-21 18:09:35 +0100

    week5 files

--- web/blog/includes/head.php
+++ web/blog/includes/head.php
@@ -9,4 +9,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="./css/style.css">
