ca618bd6520fa1aad2b46f67d5ef9fc60ce5d963
wayneherridge <wsherridge@icloud.com>
2020-05-22 21:07:47 +0100

    week5 files

--- web/blog/includes/header.php
+++ web/blog/includes/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand" href="index.php">Blog</a>
+  <a class="navbar-brand navbar-light" href="index.php">Blog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
