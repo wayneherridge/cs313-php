add38c7973a53814f42e9ef74c46e532c782963b
wayneherridge <wsherridge@icloud.com>
2020-05-22 10:31:49 +0100

    week5 files

--- web/blog/includes/header.php
+++ web/blog/includes/header.php
@@ -24,7 +24,7 @@
         <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
 	  <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us <span class="sr-only">(current)</span></a>
       </li>
 
     </ul>
