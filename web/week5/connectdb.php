a3d52b28aa6614c54e960ad1ed3907e456fc9d94
wayneherridge <wsherridge@icloud.com>
2020-05-26 19:46:32 +0100

    week5 team files

--- web/week5/connectdb.php
+++ web/week5/connectdb.php
@@ -3,38 +3,73 @@
 function get_db() {
 	$db = NULL;
 
-	try {
-		// default Heroku Postgres configuration URL
-		$dbUrl = getenv('DATABASE_URL');
+	/*
+	// It would be better to store these in a different file
+$dbUser = 'postgres';
+$dbPassword = 'Password';
+$dbName = 'Blog';
+$dbHost = 'localhost';
+$dbPort = '5432';
 
-		if (!isset($dbUrl) || empty($dbUrl)) {
-			$dbUrl = "pgsql:host=localhost;dbname=scriptureNotes";
-			$dbUser = "postgres";
-			$dbPassword = "";
-		} else {
-			// Get the various parts of the DB Connection from the URL
-		$dbopts = parse_url($dbUrl);
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
-		$dbUrl = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
-		}
+try
+{
+	// Create the PDO connection
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-		// Create the PDO connection
-		$db = new PDO($dbUrl, $dbUser, $dbPassword);
+	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex)
+{
+	// If this were in production, you would not want to echo
+	// the details of the exception.
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
 
-		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch (PDOException $ex) {
-		// If this were in production, you would not want to echo
-		// the details of the exception.
-		echo "Error connecting to DB. Details: $ex";
-		die();
-	}
+*/
 
+try {
+	// default Heroku Postgres configuration URL
+	$dbUrl = getenv('DATABASE_URL');
+
+	// if (!isset($dbUrl) || empty($dbUrl)) {
+	// 	// example localhost configuration URL with user: "ta_user", password: "ta_pass"
+	// 	// and a database called "scripture_ta"
+	// 	$dbUrl = "postgres://admin_user:admin_pass@localhost:5432/Blog";
+
+	// 	// NOTE: It is not great to put this sensitive information right
+	// 	// here in a file that gets committed to version control. It's not
+	// 	// as bad as putting your Heroku user and password here, but still
+	// 	// not ideal.
+		
+	// 	// It would be better to put your local connection information
+	// 	// into an environment variable on your local computer. That way
+	// 	// it would work consistently regardless of whether the application
+	// 	// were running locally or at heroku.
+	// }
+
+	// Get the various parts of the DB Connection from the URL
+	$dbopts = parse_url($dbUrl);
+
+	$dbHost = $dbopts["host"];
+	$dbPort = $dbopts["port"];
+	$dbUser = $dbopts["user"];
+	$dbPassword = $dbopts["pass"];
+	$dbName = ltrim($dbopts["path"],'/');
+
+	// Create the PDO connection
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex) {
+	// If this were in production, you would not want to echo
+	// the details of the exception.
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
 	return $db;
 }
 ?>
