1355c6bc0b48505cbded75679049c518e4bb6e0a
wayneherridge <wsherridge@icloud.com>
2020-05-29 19:04:10 +0100

    blog rewrite

--- web/blog/index.php
+++ web/blog/index.php
@@ -1,60 +1,18 @@
 <?php
+$uri = $_SERVER['REQUEST_URI'];
 
+// require '/core/dbconnect.php';
 
-include("config.php");
+// $db = dbconnect();
 
-$searchTerm = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_STRING);
-
-require "dbConnect.php";
-$db = get_db();
-
-if (!empty($searchTerm)) {
-    $statement = $db->prepare('SELECT * FROM posts WHERE title LIKE ?');
-    $statement->execute(array('%'.$searchTerm.'%'));
-} else {
-    $statement = $db->prepare("SELECT * FROM posts");
-    $statement->execute();
+switch ($uri) {
+    case '/':
+        $msg = "Hello World!";
+        require 'pages/home.php';
+        exit;
+        break;
+    case '/about':
+        $msg = "About World!";
+        require 'pages/about.php';
+        break;
 }
-
-    //$statement->execute();
-	$blogposts = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $statement->closeCursor();
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php include("includes/head.php");?>
-</head>
-
-<body>
-<header>
-        <?php include("includes/header.php"); ?>
-</header>
-    
-<div class="container" id="main-content">
-	<h2>Blog Posts</h2>
-
-<?php
-
-// Go through each result
-foreach ($blogposts as $row)
-    {	
-
-        echo "<p><a href='details.php?post={$row['post_id']}'><strong>{$row['title']}</strong></a><p>";
-        echo "<p>{$row['pdate']}<p>";
-
-    }
-
-?>
-
-</div>
-
-    <?php include("includes/footer.php");?>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
-</html>
