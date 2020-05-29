3f181c531db68093ab1baaf10b10b0def28854d0
wayneherridge <wsherridge@icloud.com>
2020-05-29 20:38:51 +0100

    blog rewrite

--- web/blog/admin/createAccount.php
+++ web/blog/admin/createAccount.php
@@ -14,8 +14,6 @@ if (!isset($username) || $username == ""
 	die(); // we always include a die after redirects.
 }
 
-// Let's not allow HTML in our usernames. It would be best to also detect this before
-// submitting the form and preven the submission.
 $username = htmlspecialchars($username);
 
 // Get the hashed password.
@@ -37,7 +35,7 @@ $statement->bindValue(':password', $hashedPassword);
 $statement->execute();
 
 // finally, redirect them to the sign in page
-header("Location: signIn.php");
+header("Location: admin/signIn.php");
 die(); // we always include a die after redirects. In this case, there would be no
        // harm if the user got the rest of the page, because there is nothing else
        // but in general, there could be things after here that we don't want them
