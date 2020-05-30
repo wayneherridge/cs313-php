263c20b1d86bfdc1a6fd2ff1d13ba9c29585f555
wayneherridge <wsherridge@icloud.com>
2020-05-30 18:23:28 +0100

    blog rewrite

--- web/blog/pages/post.php
+++ web/blog/pages/post.php
@@ -20,7 +20,7 @@ $query->closeCursor();
     <p><?=$blogpost['pdate'];?></p>
     <p><?=$blogpost['body'];?></p>
 
-    echo "<p><a href='index.php'>Back to Posts</a><p>";
+    echo "<p><a href='home.php'>Back to Posts</a><p>";
 
 </div>
 
