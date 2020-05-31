64d9c2293404f99342356c8b2eb12b0c4626376c
wayneherridge <wsherridge@icloud.com>
2020-05-31 01:03:26 +0100

    blog rewrite

--- web/blog/partials/header.php
+++ web/blog/partials/header.php
@@ -32,7 +32,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link <?php if ($currentPage == "Index") {?>active<?php }?>" href="home.php">Home
+                    <a class="nav-link <?php if ($currentPage == "Index") {?>active<?php }?>" href="/home">Home
                         <span class="sr-only">(current)</span></a>
                 </li>
                 <!-- <li class="nav-item active">
