763e946454f6eba0ee08381206b74dcbdd9600d6
wayneherridge <wsherridge@icloud.com>
2020-05-31 01:08:46 +0100

    blog rewrite

--- web/blog/pages/home.php
+++ web/blog/pages/home.php
@@ -15,12 +15,15 @@ $statement->closeCursor();
 
 <?php foreach ($blogposts as $post): ?>
 
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title"><?=$post['title'];?></h5>
-    <h6 class="card-subtitle mb-2 text-muted"><?=$post['pDate'];?></h6>
-    <p class="card-text"><?=$post['body'];?></p>
-    <a href="<?=$baseURI?>view-post?p=<?=$post['post_id']?>" class="card-link">View Post</a>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><?=$post['title'];?></h5>
+        <p class="card-text"><?=$post['body'];?></p>
+        <a href="<?=$baseURI?>view-post?p=<?=$post['post_id']?>" class="btn btn-primary">View Post</a>
+      </div>
+    </div>
   </div>
 </div>
 
