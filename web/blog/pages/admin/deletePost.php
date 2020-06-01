91b535650be670b153625dd7449bea483b62b710
wayneherridge <wsherridge@icloud.com>
2020-06-01 21:47:38 +0100

    blog rewrite

--- web/blog/pages/admin/deletePost.php
+++ web/blog/pages/admin/deletePost.php
@@ -1,36 +1,30 @@
 <?php
-$user = protect();
+$post_id = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
 
-$user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
-$pDate = filter_input(INPUT_POST, 'pDate', FILTER_SANITIZE_STRING);
-$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
-$body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_STRING);
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (empty($pDate) || empty($title) || empty($body)) {
-        $message = "Missing Input";
-    }
-
-    $query = $db->prepare('INSERT INTO posts (user_id, pDate, title, body) VALUES (:user_id, :pDate, :title, :body)');
-
-    $query->execute([':user_id' => $user_id, ':pDate' => $pDate, ':title' => $title, ':body' => $body]);
-
-    $result = $query->rowCount();
-
-    $query->closeCursor();
-
-    if ($result) {
-        header('Location: ' . $baseURI);
-    }
+if (empty($post_id)) {
+    die('Post does not exist');
 }
 
+$query = $db->prepare('SELECT * FROM posts WHERE post_id = :post_id');
+
+$query->execute([':post_id' => $post_id]);
+
+$blogpost = $query->fetch(PDO::FETCH_ASSOC);
+
+$query->closeCursor();
 ?>
 <?php require $basePath . '/partials/header.php';?>
-<form action="<?=$baseURI;?>delete-post" method="POST">
+
+
+
+<div>
     <h3><?=$blogpost['title'];?></h3>
     <p><?=$blogpost['pdate'];?></p>
     <p><?=$blogpost['body'];?></p>
 
-    <button type="submit" name="delete">Confirm Delete</button>
-</form>
-<?php require $basePath . '/partials/footer.php';?>
+    <a href="<?=$baseURI?>">View All Posts</a>
+    <a href="<?=$baseURI?>edit-post?p=<?=$post['post_id']?>">Edit Post</a>
+    <a href="<?=$baseURI?>delete-post?p=<?=$post['post_id']?>">Delete Post</a>
+</div>
+
+<?php require $basePath . '/partials/footer.php';
