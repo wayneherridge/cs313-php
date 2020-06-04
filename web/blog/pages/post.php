5b666f6b690ac6ca695cff784fe58d09212abe18
wayneherridge <wsherridge@icloud.com>
2020-06-04 20:49:03 +0100

    added back view all posts button

--- web/blog/pages/post.php
+++ web/blog/pages/post.php
@@ -25,6 +25,7 @@ $query->closeCursor();
     <?php if (!auth()): ?>
     <a href="<?=$baseURI?>" class="btn btn-primary btn-sm">View All Posts</a>
     <?php else: ?>
+    <a href="<?=$baseURI?>" class="btn btn-primary btn-sm">View All Posts</a>
     <a href="<?=$baseURI?>edit-post?p=<?=$blogpost['post_id']?>" class="btn btn-primary btn-sm">Edit Post</a>
     <a href="<?=$baseURI?>delete-post?p=<?=$blogpost['post_id']?>" class="btn btn-primary btn-sm">Delete Post</a>
     <?php endif;?>
